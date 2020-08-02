d98f264cc3a805546eb0b8599171af1a8f8c3e27
raghav.gupta <raghav.gupta@tothenew.com>
2020-08-02 13:26:32 +0530

    seen private properties.

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -4,19 +4,17 @@ void main() {
   runApp(MyApp());
 }
 
-//  ---------- focus -----------
 class MyApp extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => MyAppState();
+  State<StatefulWidget> createState() => _MyAppState();
 }
 
-// --------- focus -----------
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      if (questionIndex < 1) questionIndex += 1;
+      if (_questionIndex < 1) _questionIndex += 1;
     });
     print('Button clicked');
   }
@@ -34,63 +32,33 @@ class MyAppState extends State<MyApp> {
         title: Text('My First App'),
       ),
       body: Column(children: [
-        Text(questions[questionIndex]),
+        Text(questions[_questionIndex]),
         RaisedButton(
           child: Text('Orange'),
-          onPressed: answerQuestion,
+          onPressed: _answerQuestion,
         ),
         RaisedButton(
           child: Text('Red'),
-          onPressed: answerQuestion,
+          onPressed: _answerQuestion,
         ),
         RaisedButton(
           child: Text('Yellow'),
-          onPressed: answerQuestion,
+          onPressed: _answerQuestion,
         ),
         RaisedButton(
           child: Text('Green'),
-          onPressed: answerQuestion,
+          onPressed: _answerQuestion,
         ),
       ]),
     ));
   }
 }
 
-/*
- * DIFFERENCE BETWEEN STATELESS AND STATEFUL WIDGETS -> 
- * stateless widgets have only external input which is passed from the 
- * ctor while creation. they can be rebuilt if the input properties change. 
- * they are just like props in react. 
+/**
+ * here we are seeing private properties in dart - 
+ * for that we need to add _ in front of that variable or class everyehere. 
+ * where we create that and where we use that also. 
  * 
- * but stateful components are those who have these input values plus some 
- * their own properties which we call state. they can re run their build method
- * when these internal properties change. this is just like the state in react. 
- * 
- * all the concept looks same to me here like react. 
- * 
- * now, to make a stateful widget, the steps are - 
- * 
- *  1. split the class into 2. keep the old class as it is and shift all 
- *     its contents to a new class which extends State. Now the initial class
- *     will be empty so extend it with StatefulWidget. 
- *  2. add generic type to the State wali new class to connect the 2 classes. 
- *  3. override the createState() method in the StatefulWidget wali class. 
- *     same thing we do with state object in ctor and the useState() hook in react. 
- *  note that, we return an object of State<StatefulWidget> from the createState 
- *  method and our own new class is of type State<StatefulWidget> also. 
- * 
- *  now, we connected the 2 classes. 
- * 
- *  ## why do we keep the build method in State wali class ??
- * - because the widget will be recreated multiple times but the state is 
- *   persistent. so, it remains the same and preserved. so, we separate them. 
- *   PROPER LOGIC NOT CLEAR - SEE LATER. 
- * 
- *  ## we need to use the setState method to update the state. Same reason as 
- *  react that otherwise it will not re-render the widget. 
- *  Whenever setState is called, it calls the build method again in the widget 
- *  where setSatte is called. and then it build the widget again. but it will 
- *  not re render the complete app. it also has an internal 
- *  mechanism which detects what has actually changed and needs to be re rendered. 
- *  just as we have a mechanism in react. so the concepts are same. 
+ * private here means that the class or var can be accessed from within this
+ * file only.
  */
